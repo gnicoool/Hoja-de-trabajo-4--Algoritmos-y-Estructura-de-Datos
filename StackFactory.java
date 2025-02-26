cc5f51bb199827392223029ba3cff5036ae54436
stan-2021131 <sergiocoromac@gmail.com>
2025-02-26 16:12:30 -0600

    Se agregó la clase StackFactory para crear distintos tipos de eestructuras de datos lienales

---
+++ StackFactory.java
@@ -0,0 +1,20 @@
+public class StackFactory {
+    //Método estático para crear clases que implementen la interfaz de IStack
+    @SuppressWarnings("rawtypes")
+    public static IStack CreateStack(String pila) throws ClassNotFoundException{
+        //Dependiendo el string enviado, crea estructura de datos lineal distinta
+        switch (pila) {
+            case "arraylist":
+                return new AL<>();
+            //Casos de las demás pilas AÚN SIN IMPLEMENTAR
+            /* case "vector":
+                return new Vec();
+            case "listaSimple":
+                return new listaSimple();
+            case "listaDoble":
+                return new listaDoble(); */
+            default:
+                throw new ClassNotFoundException(pila);
+        }
+    }
+}
