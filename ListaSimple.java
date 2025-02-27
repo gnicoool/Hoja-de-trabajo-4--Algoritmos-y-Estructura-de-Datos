c0aeec74a6332442511e4ea0f07ab72ce588dd44
unknown <alejandraviles2005@gmail.com>
2025-02-27 10:59:28 -0600

    Se trabajó en todos los elementos necesarios para la clase ListaSimple.

--- ListaSimple.java
+++ ListaSimple.java
@@ -1,24 +1,57 @@
 public class ListaSimple<T extends Number> implements IStack<T>{
+    private Node<T> cabeza;
+    private int size;
+
+    public ListaSimple() {
+        this.cabeza = null;
+        this.size = 0;
+    }
 
     @Override
     public void add(T value) {
-
+        Node<T> nuevo = new Node<>(value);
+        if (this.cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Node<T> temp = cabeza;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(nuevo);
+        }
+    size++;
     }
 
     @Override
     public void clear() {
+        cabeza = null;
+        size = 0;
     }
 
     @Override
     public T pop() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+        if (cabeza == null) {
+            return null;
+        }
+        if (cabeza.getNext() == null){
+            T value = cabeza.getValue();
+            cabeza = null;
+            size--;
+            return value;
+        }
+        Node<T> temp = cabeza;
+        while (temp.getNext().getNext() != null) {
+            temp = temp.getNext();
+        }
+        T value = temp.getNext().getValue();   
+        temp.setNext(null);
+        size--;
+        return value;
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return size;
     }
     
 }
